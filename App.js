bca39d92e0eb51a9e65cb20dfe33b635a8646b10
Ofallbrains <ofallbrains@gmail.com>
2024-02-13 22:28:20 +0200

    React native projects

--- App.js
+++ App.js
@@ -1,58 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, TouchableHighlight, TouchableOpacity} from 'react-native';
+import {View,} from 'react-native';
+
+import {Signin} from "./Screen/Signin";
 
 export default function App() {
-  
-  return (
-    <View style={styles.container}>
-      <Text style={{marginTop: 30, backgroundColor: '#FF5100', width: '100%', height: 90, textAlign: 'right', fontSize: 35, padding:25, color: 'white'}}>Todo</Text>
-      <Text style={{ paddingTop: 60, fontSize: 30, width: 300, marginBottom: 10}}>Todo list items</Text>
-  
-      <View>
-        <View>
-          <TouchableOpacity style={{display:'flex', flexDirection:'row', gap: 10, borderWidth: 1, bordercolor: 'black', padding: 18, width: 300,borderRadius: 10,borderStyle: 'dashed',marginTop: 30,}}>
-            <Text style={{fontSize: 25}}>1</Text>
-            <Text style={{fontSize: 25, fontWeight:'light'}}>Cleaning</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity style={{display:'flex', flexDirection:'row', gap: 10, borderWidth: 1, bordercolor: 'black', padding: 18, width: 300,borderRadius: 10,borderStyle: 'dashed',marginTop: 30}}>
-            <Text style={{fontSize: 25}}>1</Text>
-            <Text style={{fontSize: 25, fontWeight:'light'}}>Cleaning</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity style={{display:'flex', flexDirection:'row', gap: 10, borderWidth: 1, bordercolor: 'black', padding: 18, width: 300,borderRadius: 10,borderStyle: 'dashed',marginTop: 30,}}>
-            <Text style={{fontSize: 25}}>1</Text>
-            <Text style ={{fontSize: 25, fontWeight:'light'}}>Cleaning</Text>
-          </TouchableOpacity>
-        </View>
-        <View> 
-          <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', gap: 10, borderWidth: 1, bordercolor: 'black', padding: 18, width: 300, borderRadius: 10, borderStyle: 'dashed', marginTop: 30}}>
-            <Text style={{fontSize: 25}}>1</Text>
-            <Text style={{fontSize: 25, fontWeight:'light'}}>Cleaning</Text>
-          </TouchableOpacity>
-        </View>
 
-      </View>  
-      <View style={{display:'flex', flexDirection:'row', marginTop: 30, gap: 110,width: 300}}>
-        <TextInput value='enter new todo item' style={{ borderColor: 'black', borderWidth: 0,borderRadius: 10, borderBottomWidth:1}}></TextInput>
-        <TouchableOpacity style={{backgroundColor:'orange',padding: 15, borderRadius: 10, color: 'white', fontSize: 20}}>
-<Text>add todo</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
-     
-    </View>
-  );
+  return (
+<View>
+      
+  <Signin />
+</View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  
-});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     // flex: 1,
+//     backgroundColor: 'white',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+
+// });
---
+++ Screen/Signin.js
@@ -0,0 +1,67 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react'
+import { CheckBox, Icon, } from "react-native-elements";
+import { AntDesign, Entypo, MaterialCommunityIcons, MaterialIcons } from "react-native-vector-icons"
+
+export const Signin = () => {
+    return (
+        <View>
+
+            {/* <View style={{ alignItems: 'center' }}> */}
+            <TouchableOpacity>
+                <Entypo name='cross' size={20} style={{ marginTop: 50, marginLeft: 30, }} />
+            </TouchableOpacity>
+            <View style={{ alignItems: 'center' }}>
+
+                <Text style={{ marginTop: 5, fontSize: 25, fontWeight: '500' }}>Sign ln</Text>
+                <Text style={{ marginTop: 10, fontWeight: '300', fontSize: 15 }}>Welcome back! Please enter your details</Text>
+            </View>
+            <View style={{ marginTop: 50, alignItems: 'center' }}>
+                <View style={{ display: 'flex', flexDirection: 'row', gap: 10, width: 300, borderWidth: 1, borderColor: '#EFECEC', borderRadius: 20, padding: 15, backgroundColor: '#EEF5FF' }}>
+                    <MaterialCommunityIcons name='email-outline' size={20} color={"red"} />
+                    <TextInput
+                        placeholder="quickrackibull@gmail.com" style={{ color: 'black' }}
+                    />
+                </View>
+                <View style={{ display: 'flex', flexDirection: 'row', gap: 10, width: 300, borderWidth: 1, borderColor: '#EFECEC', borderRadius: 20, padding: 15, marginTop: 10, backgroundColor: '#EEF5FF' }}>
+                    <MaterialIcons name='lock-outline' size={20} color={"#40A2E3"}/>
+                    <TextInput secureTextEntry
+                        placeholder="Password"
+                    />
+                </View>
+            </View>
+            <View style={{ }}>
+
+                <CheckBox title="Remember" containerStyle={{ backgroundColor: 'white', borderColor: 'white', marginLeft: 30, fontWeight: '300', }} />
+            </View>
+            
+            <View style={{ marginTop: 60, alignItems: 'center' }}>
+                <Text style={{ color: '#40A2E3', padding: 10, fontWeight: '500', fontSize: 18 }}>Forgot Password</Text>
+                <TouchableOpacity>
+                    <Text style={{ backgroundColor: '#40A2E3', color: 'white', borderRadius: 20, padding: 15, width: 290, textAlign: 'center', fontSize: 20, fontWeight: '500' }}>Sign ln</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{ marginTop: 25, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <TouchableOpacity>
+                    <Text style={{ fontSize: 15, fontWeight: '300', marginLeft: 40 }}>Create account</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={{ marginRight: 40, color: 'orange', borderWidth: 1, borderColor: '#EEEDEB', backgroundColor: '#EEEDEB', borderRadius: 15, padding: 5, width: 70, textAlign: 'center', fontSize: 15, fontWeight: '400' }}>Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: 40, }}>
+
+                <Icon name="facebook" size={30} color="blue" style={{ borderWidth: 1, borderColor: 'white', borderRadius: 80, padding: 10, backgroundColor: '#EEF5FF', marginLeft: 20 }} />
+                <AntDesign name='google' size={24} color="red" style={{ borderWidth: 1, borderColor: 'white', borderRadius: 80, padding: 10, backgroundColor: '#EEF5FF' }} />
+                <Icon name="apple" size={30} color="black" style={{ borderWidth: 1, borderColor: 'white', borderRadius: 80, padding: 10, backgroundColor: '#EEF5FF', marginRight: 30 }} />
+            </View>
+
+
+
+            <StatusBar style="auto" />
+        </View>
+
+
+    )
+}
